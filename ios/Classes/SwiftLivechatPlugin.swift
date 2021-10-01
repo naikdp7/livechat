bb9c806030040b3c9cdf7d03095d9a444c4a2b41
Deep Naik <naikdp7@gmail.com>
2021-10-01 22:05:17 +0530

    ->import changes

--- ios/Classes/SwiftLivechatPlugin.swift
+++ ios/Classes/SwiftLivechatPlugin.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-import LiveChat
+import LiveChatFlutter
 
 public class SwiftLivechatPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
